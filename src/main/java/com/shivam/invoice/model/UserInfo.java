27ec5bcc3fd3b77d4c84f05956bd68185288744f
Shivam Verma <shivam.r.verma@seagate.com>
2022-11-13 15:56:08 +0530

    Working on adding products
    Signed-off-by: Shivam Verma <shivam.r.verma@seagate.com>

--- src/main/java/com/shivam/invoice/model/UserInfo.java
+++ src/main/java/com/shivam/invoice/model/UserInfo.java
@@ -1,5 +1,7 @@
 package com.shivam.invoice.model;
 
+import java.util.ArrayList;
+
 import org.springframework.stereotype.Component;
 
 @Component
@@ -11,6 +13,17 @@ public class UserInfo {
     private String orderDate;
     private String orderNumber;
 
+    private String billingAddress;
+    private String billingCity;
+    private String billingPincode;
+    private String billingState;
+
+    private String shippingAddress;
+    private String shippingCity;
+    private String shippingPincode;
+    private String shippingState;
+
+    private ArrayList<Product> products;
  
     public UserInfo() {
     }
@@ -61,7 +74,80 @@ public class UserInfo {
         this.orderNumber = orderNumber;
     }
 
+    public String getBillingAddress() {
+        return billingAddress;
+    }
 
+    public void setBillingAddress(String billingAddress) {
+        this.billingAddress = billingAddress;
+    }
+
+    public String getBillingCity() {
+        return billingCity;
+    }
+
+    public void setBillingCity(String billingCity) {
+        this.billingCity = billingCity;
+    }
+
+    public String getBillingPincode() {
+        return billingPincode;
+    }
+
+    public void setBillingPincode(String billingPincode) {
+        this.billingPincode = billingPincode;
+    }
+
+    public String getBillingState() {
+        return billingState;
+    }
+
+    public void setBillingState(String billingState) {
+        this.billingState = billingState;
+    }
+
+    public String getShippingAddress() {
+        return shippingAddress;
+    }
+
+    public void setShippingAddress(String shippingAddress) {
+        this.shippingAddress = shippingAddress;
+    }
+
+    public String getShippingCity() {
+        return shippingCity;
+    }
+
+    public void setShippingCity(String shippingCity) {
+        this.shippingCity = shippingCity;
+    }
+
+    public String getShippingPincode() {
+        return shippingPincode;
+    }
+
+    public void setShippingPincode(String shippingPincode) {
+        this.shippingPincode = shippingPincode;
+    }
+
+    public String getShippingState() {
+        return shippingState;
+    }
+
+    public void setShippingState(String shippingState) {
+        this.shippingState = shippingState;
+    }
+
+    public ArrayList<Product> getProducts() {
+        return products;
+    }
+
+    public void setProducts(ArrayList<Product> products) {
+        this.products = products;
+    }
+
+
+    
 
     
 }
--- src/main/java/com/shivam/invoice/services/EmailService.java
+++ src/main/java/com/shivam/invoice/services/EmailService.java
@@ -1,19 +1,33 @@
 package com.shivam.invoice.services;
 
+import java.io.File;
 import java.io.IOException;
 import java.io.StringWriter;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Properties;
 
+import javax.activation.DataHandler;
+import javax.mail.Authenticator;
+import javax.mail.BodyPart;
 import javax.mail.MessagingException;
+import javax.mail.Multipart;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Message.RecipientType;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeBodyPart;
 import javax.mail.internet.MimeMessage;
+import javax.mail.internet.MimeMultipart;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.mail.javamail.MimeMessagePreparator;
 import org.springframework.stereotype.Component;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.shivam.invoice.helper.ResponseMessage;
 import com.shivam.invoice.model.UserInfo;
@@ -42,7 +56,17 @@ public class EmailService {
             helper.setTo(user.getEmail());
             String emailContent = getEmailContent(user);
             helper.setText(emailContent, true);
+            // File file = new File(emailContent);
+            // // Byte
+            // // helper.addAttachment(emailContent,null);
+            // helper.addAttachment("invoice",file);
+            // Session session = Session.getInstance(propert, null)
+
+            
+
             javaMailSender.send(mimeMessage);
+
+
             responseMessage.setMessage("Email Sent");
             return ResponseEntity.status(HttpStatus.OK).body(responseMessage);
         } catch (Exception e) {
@@ -55,12 +79,22 @@ public class EmailService {
 
     String getEmailContent(UserInfo user) throws IOException, TemplateException {
         StringWriter stringWriter = new StringWriter();
-        Map<String, String> model = new HashMap<>();
-        model.put("userName", user.getName());
-        model.put("userEmail", user.getEmail());
-        model.put("invoiceId",user.getInvoiceId());
-        model.put("orderDate", user.getOrderDate());
-        model.put("orderNumber",user.getOrderNumber());
+        Map<String, Object> model = new HashMap<>();
+        model.put("user",user);
+        // model.put("userName", user.getName());
+        // model.put("userEmail", user.getEmail());
+        // model.put("invoiceId",user.getInvoiceId());
+        // model.put("orderDate", user.getOrderDate());
+        // model.put("orderNumber",user.getOrderNumber());
+        // model.put("billingAddress",user.getBillingAddress());
+        // model.put("billingCity",user.getBillingCity());
+        // model.put("billingPincode",user.getBillingPincode());
+        // model.put("billingState",user.getBillingState());
+        // model.put("shippingAddress",user.getShippingAddress());
+        // model.put("shippingCity",user.getShippingCity());
+        // model.put("shippingPincode",user.getShippingPincode());
+        // model.put("shippingState",user.getShippingState());
+        // model.put("products",user.getProducts());
         
         configuration.getTemplate("email.ftlh").process(model, stringWriter);
         return stringWriter.getBuffer().toString();
